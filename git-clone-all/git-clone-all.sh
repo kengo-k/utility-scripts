58ddebdf4c0b1b35e3bfd4974d8f0ba54f71875e
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-24 19:40:17 +0900

    init

---
+++ git-clone-all/git-clone-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the necessary tools are installed
+command -v gh >/dev/null 2>&1 || { echo >&2 "gh is not installed. Aborting."; exit 1; }
+command -v ghq >/dev/null 2>&1 || { echo >&2 "ghq is not installed. Aborting."; exit 1; }
+
+# Load GitHub token from file
+if [[ -f "github_token" ]]; then
+    GITHUB_TOKEN=$(cat github_token)
+else
+    echo "File 'github_token' not found. Aborting."
+    exit 1
+fi
+
+# Authenticate with GitHub
+gh auth login --with-token <<< $GITHUB_TOKEN
+
+# Get a list of all repositories for the authenticated user
+repos=$(gh repo list --json nameWithOwner -q '.[] | .nameWithOwner')
+
+# Clone all the repositories with ghq
+for repo in $repos; do
+    ghq get "github:$repo"
+done
